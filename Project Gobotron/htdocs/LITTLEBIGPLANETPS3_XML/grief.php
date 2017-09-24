728be0eca6aea3b29abe4bed836f8463619b20e0
TBA <a50caloverlord@gmail.com>
2017-09-24 23:49:32 +0100

    grief report php

---
+++ Project Gobotron/htdocs/LITTLEBIGPLANETPS3_XML/grief.php
@@ -0,0 +1,10 @@
+<?php header('Content-Type: text/xml');?>
+<griefReport>
+<levelOwner>Infrnyl</levelOwner>
+<initialStateHash>370cc43b704ba440169da8995ba91c666cd77ed6</initialStateHash>
+<levelType>pod</levelType>
+<levelId>0</levelId>
+<griefStateHash>f85bdac6cfef743c0edc54e36bd61fc9a59d299a</griefStateHash>
+<player reporter="true" ingamenow="true" playerNumber="1"><id>Infrnyl</id><rect t="285" l="299" b="322" r="338"/></player>
+<jpegHash>217b1eceb296c0d44c5bda216124bf67b8abcacd</jpegHash>
+<griefTypeId>8</griefTypeId><marqee><rect t="0" l="0" b="119" r="163"/></marqee></griefReport>
